5c2c830e676c32a3e7e16bab9ebc5ac062c6e1fe
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2024-10-07 18:29:59 +0700

    Renaming Functions And Adding Some Features
    - Renamed all functions
    - (New feature) Added Auto Delete Script If Error, Killed, etc.
    - (New feature) Added Network Testing, If Offline Then This Script Will Be Deleted Automatically
    - Some optimizations

--- termuxsetup.sh
+++ termuxsetup.sh
@@ -3,54 +3,76 @@
 # Script Information
 SCRIPT_NAME="Termux Setup"
 SCRIPT_VERSION="1.0"
-SCRIPT_VERSION_BUILD="241071744"
+SCRIPT_VERSION_BUILD="241071825"
 DEVELOPER="Ringga"
 DEV_USERNAME="@ringgarevanka"
 
-# Enable strict error handling: 'set -e' exits on errors, 'set -u' treats unset variables as errors
-set -eu
+# This script initializes a safe execution environment.
+initialize_environment() {
+    # Enable strict error handling:
+    # 'set -e' will exit the script if any command fails
+    # 'set -u' will treat unset variables as errors
+    set -eu
 
-# DISABLE CTRL+Z
-trap '' SIGTSTP
+    # Trap to disable the CTRL+Z key combination
+    trap '' SIGTSTP
+
+    # Trap for errors, signals, and network issues:
+    # The script will automatically delete itself if an error occurs or if it receives SIGINT, SIGTERM, or SIGHUP signals
+    trap 'rm -rf "$0"; exit 1' ERR SIGINT SIGTERM SIGHUP
+
+    # Function to check network availability:
+    # If the network is unavailable (cannot ping 8.8.8.8),
+    # this script will delete itself and exit with status 1
+    check_network() {
+        if ! ping -c 1 8.8.8.8 &> /dev/null; then
+            rm -rf "$0"
+            exit 1
+        fi
+    }
+
+    # Call the function to check network connectivity
+    check_network
+}
 
 # ANSI color codes for formatting
 RESET="\033[0m"
 
 # Green Text
-echo_green() {
+display_in_green() {
     local COLOR="\033[1;32m"
     echo -e "${COLOR}$1${RESET}"
 }
 
 # Function to display header with script information
-display_header() {
+show_header() {
     clear
-    echo_green "$SCRIPT_NAME $SCRIPT_VERSION ($SCRIPT_VERSION_BUILD) By: $DEVELOPER ($DEV_USERNAME)"
+    display_in_green "$SCRIPT_NAME $SCRIPT_VERSION ($SCRIPT_VERSION_BUILD) By: $DEVELOPER ($DEV_USERNAME)"
 }
 
 # Function to display a message in green color
-display_message() {
+show_message() {
     local message="$1"
-    display_header
-    echo_green "$message"
-    echo_green ""
+    show_header
+    display_in_green "$message"
+    display_in_green ""
 }
 
 # Function to update and upgrade packages
-update_and_upgrade() {
-    display_message "Updating and Upgrading Packages..."
+perform_update_and_upgrade() {
+    show_message "Updating and Upgrading Packages..."
     pkg update -y && pkg upgrade -y
 }
 
-# Function to setup storage using 'termux-setup-storage'
-setup_storage() {
-    display_message "Set up Storage..."
+# Function to set up storage using 'termux-setup-storage'
+configure_storage() {
+    show_message "Setting up Storage..."
     termux-setup-storage
 }
 
 # Function to install a list of essential packages
-install_packages() {
-    local termux_repo=(
+install_essential_packages() {
+    local termux_repositories=(
         x11-repo root-repo science-repo game-repo tur-repo
     )
 
@@ -58,22 +80,33 @@ install_packages() {
         termux-am termux-am-socket termux-api termux-api-static termux-apt-repo termux-auth termux-elf-cleaner termux-exec termux-keyring termux-licenses termux-services termux-tools
     )
 
-    local packages=(
+    local additional_packages=(
         git curl wget nodejs-lts python-pip python python-tkinter python-numpy electrum opencv-python asciinema matplotlib python-cryptography openssl libffi libcrypt clang perl php sqlite zsh nano shfmt dnsutils htop jq grep ffmpeg openssh pulseaudio fakeroot bc tsu fastfetch android-tools zip unzip proot-distro
     )
 
-    # Loop and install
-    for package in "${termux_repo[@]}" "${termux_packages[@]}" "${packages[@]}"; do
-        display_message "Installing or update termux repository, termux packages, and essential packages..."
-        echo_green "Installing or Upgrade '$package'..."
+    # Loop and install each package
+    for package in "${termux_repositories[@]}"; do
+        show_message "Adding Termux repository '$package'..."
         pkg install -y "$package"
-        echo_green "Success Installing or Upgrade $package"
+        display_in_green "Successfully added $package"
+    done
+
+    for package in "${termux_packages[@]}"; do
+        show_message "Installing Termux package '$package'..."
+        pkg install -y "$package"
+        display_in_green "Successfully installed $package"
+    done
+
+    for package in "${additional_packages[@]}"; do
+        show_message "Installing additional package '$package'..."
+        pkg install -y "$package"
+        display_in_green "Successfully installed $package"
     done
 }
 
 # Function to install PHP Composer with signature verification for security
 setup_php_composer() {
-    display_message "Setting up PHP Composer..."
+    show_message "Setting up PHP Composer..."
 
     local EXPECTED_SIGNATURE
     EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
@@ -85,6 +118,7 @@ setup_php_composer() {
     if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]; then
         php composer-setup.php --install-dir=/data/data/com.termux/files/usr/bin --filename=composer
         rm composer-setup.php
+        display_in_green "PHP Composer installed successfully."
     else
         echo "ERROR: Invalid installer signature"
         rm composer-setup.php
@@ -93,24 +127,23 @@ setup_php_composer() {
 }
 
 # Function to install additional Python packages
-setup_python_packages() {
+install_additional_python_packages() {
     local packages=(
         setuptools httpie beautifulsoup4 numpy electrum asciinema matplotlib
     )
 
     for package in "${packages[@]}"; do
-        display_message "Installing additional Python packages..."
-        echo_green "Installing or Upgrade '$package'..."
+        show_message "Installing additional Python package '$package'..."
         pip install --quiet --upgrade "$package"
-        echo_green "Success Installing or Upgrade $package"
+        display_in_green "Successfully installed or updated $package"
     done
 }
 
 # Function to customize the Termux interface
-customize_interface() {
-    display_message "Customizing Termux Interface..."
+customize_termux_interface() {
+    show_message "Customizing Termux Interface..."
 
-    # Custom .bashrc file
+    # Create a custom .bashrc file for user settings
     cat <<EOF >"$HOME/.bashrc"
 # Optimized Termux Setup
 clear
@@ -118,7 +151,7 @@ termux-reload-settings
 fastfetch -l none
 EOF
 
-    # Custom termux.properties for extra keys
+    # Create a custom termux.properties file for extra keys
     mkdir -p "$HOME/.termux"
     cat <<EOF >"$HOME/.termux/termux.properties"
 # Optimized extra-keys layout
@@ -126,73 +159,79 @@ extra-keys = [[{key: 'ESC', popup: {macro: 'CTRL d', display: 'EXIT'}},{key: '/'
 EOF
 }
 
-# Function to perform termux cleanup
-clean_termux() {
-    display_message "Cleaning Termux..."
+# Function to clean up the Termux environment
+perform_termux_cleanup() {
+    show_message "Cleaning Termux environment..."
     local DIR="/data/data/com.termux/"
 
-    # Cleaning pkg and APT cache and removing unused packages
-    echo_green "Cleaning PKG and APT cache, and removing unused packages..."
-    # APT
+    # Clean up package and APT cache, and remove unused packages
+    display_in_green "Cleaning package and APT cache, and removing unused packages..."
     apt autoremove -y --purge
     apt autoclean
     apt clean
-    # PKG
     pkg autoclean
     pkg clean
-    echo_green "PKG and APT cleanup completed.\n"
+    display_in_green "Package and APT cleanup completed.\n"
 
-    # Removing .bak and .tmp files
-    echo_green "Removing .bak and .tmp files..."
+    # Remove .bak and .tmp files
+    display_in_green "Removing .bak and .tmp files..."
     find "$DIR" -type f -name "*.bak" -o -name "*.tmp" -delete
-    echo_green ".bak and .tmp files deleted.\n"
+    display_in_green ".bak and .tmp files deleted.\n"
 
-    # Cleaning cache and tmp directories
-    echo_green "Cleaning cache and tmp directories..."
+    # Clean cache and temporary directories
+    display_in_green "Cleaning cache and temporary directories..."
     find "$DIR" -type d -name ".cache" -o -name ".tmp" -exec rm -rf {} +
-    echo_green "Cache and tmp directories cleaned.\n"
+    display_in_green "Cache and temporary directories cleaned.\n"
 
-    # Cleaning Termux-specific temp directories
-    echo_green "Cleaning Termux cache and tmp directories..."
+    # Clean Termux-specific temporary directories
+    display_in_green "Cleaning Termux cache and temporary directories..."
     rm -rf "$HOME/.termux/tmp/*"
-    echo_green "Termux cache and tmp directories cleaned.\n"
+    display_in_green "Termux cache and temporary directories cleaned.\n"
 
-    # Clearing log files
-    echo_green "Clearing log files..."
+    # Clear log files
+    display_in_green "Clearing log files..."
     find "$DIR" -type f -name "*.log" -delete
-    echo_green "Log files deleted.\n"
+    display_in_green "Log files deleted.\n"
 
-    echo_green "Termux cleanup completed successfully."
+    display_in_green "Termux cleanup completed successfully."
+}
+
+# Function to clear traps and exit safely
+exit_cleanly() {
+    # Remove trap to ENABLE CTRL+Z
+    trap - SIGTSTP
+
+    # Remove traps for errors, signals, and network problems
+    trap - ERR SIGINT SIGTERM SIGHUP
+
+    # Remove this script
+    rm -rf "$0"
+
+    # Exit the script
+    exit 0
 }
 
 # Main function to orchestrate the setup
 main() {
-    display_header
+    show_header
 
-    update_and_upgrade
-    setup_storage
+    perform_update_and_upgrade
+    configure_storage
 
-    install_packages
+    install_essential_packages
     setup_php_composer
-    setup_python_packages
+    install_additional_python_packages
 
-    customize_interface
-    clean_termux
+    customize_termux_interface
+    perform_termux_cleanup
 
-    display_message "Reloading settings..."
+    show_message "Reloading settings..."
     termux-reload-settings
 
-    display_message "Setup completed!"
+    show_message "Setup completed!"
 }
 
 # Execute the main function
+initialize_environment
 main
-
-# ENABLE CTRL+Z
-trap - SIGTSTP
-
-# Remove This Script
-rm -rf "$0"
-
-# Exit
-exit
+exit_cleanly
